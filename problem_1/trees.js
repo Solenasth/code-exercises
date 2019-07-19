2fbecd81bdbd6ca1a6e284fa5d771c6a0dda3b82
carlos alcala <solenasthellion@gmail.com>
2019-07-19 17:42:10 -0500

    :rocket: problem solved for problem 1 and 2

---
+++ problem_1/trees.js
@@ -0,0 +1,46 @@
+// -1 = 🌳
+// > -1 = 👨
+// sort 👨 but not 🌳 by height values
+// test_input = [-1, 150, 190, 170, -1, -1, 160, 180]
+// expected_output = [-1, 150, 160, 170, -1, -1, 180, 190]
+
+
+//solucion 1: filter(>0) , sort , assign back 
+
+let test_input =  [-1, 150, 190, 170, -1, -1, 160, 180]; 
+
+function getPeople(TreesAndPeeps){
+  return TreesAndPeeps.filter(element => element>-1); 
+}
+
+function sortPeople(Peeps){
+    return Peeps.slice().sort((a,b)=>a-b);
+}
+
+function getSortedPeople(test_input){
+    return sortPeople(getPeople(test_input));
+}
+
+function reasignPeople(input){
+    let sorted = getSortedPeople(input); 
+    sorted
+    input
+    return input.map((item) => {
+       return (item > 0) ? 
+         sorted.shift() : item 
+    });
+}
+
+
+// this function is just graphical sugar it does nothing functional and can be ignored.
+function sugar(result){
+    return result.map((item)=>{
+       return (item < 0) ? 
+       "🌲" : ("🧔 " + item +"cm")
+    })
+}
+
+console.log(reasignPeople(test_input));
+console.log(sugar(reasignPeople(test_input)));
+
+
---
+++ problem_2/strings.js
@@ -0,0 +1,33 @@
+let input_value = "foo(bar(baz))blim";
+let expresion = '(\\(([^()]*)\\))';
+const regex = RegExp(expresion);
+
+function revert(input_string){
+    return input_string.split('').reverse().join('');
+}
+
+function clean(input_string){
+    return input_string.slice().replace(/[()]/gi,'');
+}
+
+function getCleanReversedString(match){
+    return revert(clean(match));
+}
+function step(input_string){
+    return input_string.replace(regex,getCleanReversedString);
+}
+
+function recursiveProcessing(target_string){
+    while(regex.test(target_string)){
+       target_string = step(target_string);
+        recursiveProcessing(target_string);
+    }
+    return target_string;
+}
+
+console.log(recursiveProcessing(input_value))
+
+
+
+
+// regex to get clean parentesis: (\(([^()]*)\))
